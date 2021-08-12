82031db60efc94d32c5d9ef259e0e847e217d85d
taylor herbots <therbots@live.com>
2021-08-12 14:30:24 -0500

    added functions set_name, chosne_gesture and set_score to Ai class

--- ai.py
+++ ai.py
@@ -1,5 +1,17 @@
 from player import Player
+from random import Random
 
 class Ai:
     def __init__(self):
-        pass
+        super().__init__()
+
+    def set_name(self):
+        self.name = 'Player 2'
+
+    def chosen_gesture(self):
+        self.chosen_gesture = Random.randint(0, 4)
+
+    def set_score(self):
+        self.score += 1
+
+    
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-
+from human import Human
+from ai import Ai
 
 class Game:
     def __init__(self):
