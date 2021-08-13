bb9bb2c3f6965f30f2717466fe27a400683cc736
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-13 12:07:13 -0400

    Changed position of set name below super call

--- human.py
+++ human.py
@@ -2,8 +2,8 @@ from player import Player
 
 class Human(Player):
     def __init__(self):
-        self.set_name()
         super().__init__()
+        self.set_name()
         
 
     def set_name(self):
